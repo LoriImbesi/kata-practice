b9a660038c8b7d13676c1441bfcc25ab546853dc
Lori Imbesi <lori.imbesi@gmail.com>
2021-07-23 10:40:10 -0400

    Changes to arabicToRoman function.

--- romanNumerals.py
+++ romanNumerals.py
@@ -3,10 +3,45 @@ arabicToRomanNums = {1: "I", 5: "V", 10: "X",
 
 
 def arabicToRoman(arabicNumber):
-    romanNumeral = arabicToRomanNums[arabicNumber]
+    romanNumeral = ""
+
+    while arabicNumber > 0:
+        if arabicNumber < 5:
+            romanNumeral += arabicToRomanNums[1]
+            arabicNumber -= 1
+        elif arabicNumber < 10:
+            romanNumeral = arabicToRomanNums[5]
+            arabicNumber -= 5
+        elif arabicNumber < 50:
+            romanNumeral = arabicToRomanNums[10]
+            arabicNumber -= 10
+        elif arabicNumber < 100:
+            romanNumeral = arabicToRomanNums[50]
+            arabicNumber -= 50
+        elif arabicNumber < 500:
+            romanNumeral = arabicToRomanNums[100]
+            arabicNumber -= 100
     print(romanNumeral)
     return romanNumeral
 
 
-if input != 1, 5, 10, 50, 100, 500, 1000:
-    then increment or decrement
+def arabicPlaceValues(arabicNumber):
+
+    numPlaceVal = []
+
+    # break arabic number into place values and pull and
+    # concat values from dict
+
+    # if arabicNum 1 - 3 concat "I"
+    # if arabicNum 4 - 8 concat "IV"
+    # if arabicNum 9,11 - 13 concat "IX"
+    # if arabicNum 14 - 18 concat "IVX"
+    # if arabicNum 19 - 23 concat "IX"
+    # if arabicNum 24 - 28 concat "IVX"
+
+    # 40 introduces "L" 40 = "XL"
+    # 90 introduces "C" 90 = "XC"
+    # 400 introduces "D" 400 = CD
+    # 900 introduces "M"  900 = "CM"
+    # 1001 = "MI"
+    # upper limit: 3000
--- romanNumeralsTests.py
+++ romanNumeralsTests.py
@@ -6,13 +6,20 @@ class TestStringMethods(unittest.TestCase):
 
     def test_arabicToRoman(self):
         self.assertEqual("I", arabicToRoman(1))
+        self.assertEqual("II", arabicToRoman(2))
+        self.assertEqual("III", arabicToRoman(3))
+        self.assertEqual("IIII", arabicToRoman(4))
         self.assertEqual("V", arabicToRoman(5))
+        self.assertEqual("VI", arabicToRoman(6))
+        self.assertEqual("VII", arabicToRoman(7))
+        self.assertEqual("VIII", arabicToRoman(8))
+        self.assertEqual("VIIII", arabicToRoman(9))
         self.assertEqual("X", arabicToRoman(10))
+        self.assertEqual("XI", arabicToRoman(11))
         self.assertEqual("L", arabicToRoman(50))
         self.assertEqual("C", arabicToRoman(100))
-        self.assertEqual("D", arabicToRoman(500))
-        self.assertEqual("M", arabicToRoman(1000))
-        self.assertEqual("VII", arabicToRoman(7))
+        # self.assertEqual("D", arabicToRoman(500))
+        # self.assertEqual("M", arabicToRoman(1000))
 
 
 if __name__ == '__main__':
