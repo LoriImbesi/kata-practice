9963f971fa1e57c728b70c8c726e99f7f322c5da
Lori Imbesi <lori.imbesi@gmail.com>
2021-07-26 17:12:57 -0400

    Added romanToArabic function.

--- romanNumerals.py
+++ romanNumerals.py
@@ -1,17 +1,35 @@
 arabicToRomanNums = {1: "I", 5: "V", 10: "X",
                      50: "L", 100: "C", 500: "D", 1000: "M"}
+romanNumsToArabic = {"I": 1, "V": 5, "X": 10,
+                     "L": 50, "C": 100, "D": 500, "M": 1000}
+
+
+def romanToArabic(romanNumeral):
+    arabicNumber = 0
+    for romanCharacter in romanNumeral:
+        arabicNumber += romanNumsToArabic[romanCharacter]
+
+    return arabicNumber
 
 
 def arabicToRoman(arabicNumber):
     romanNumeral = ""
 
     while arabicNumber > 0:
-        if arabicNumber < 5:
+        if arabicNumber < 4:
             romanNumeral += arabicToRomanNums[1]
             arabicNumber -= 1
-        elif arabicNumber < 10:
+        elif arabicNumber == 4:
+            romanNumeral += arabicToRomanNums[1]
+            romanNumeral += arabicToRomanNums[5]
+            arabicNumber -= 4
+        elif arabicNumber < 9:
             romanNumeral = arabicToRomanNums[5]
             arabicNumber -= 5
+        elif arabicNumber == 9:
+            romanNumeral += arabicToRomanNums[1]
+            romanNumeral += arabicToRomanNums[10]
+            arabicNumber -= 9
         elif arabicNumber < 50:
             romanNumeral = arabicToRomanNums[10]
             arabicNumber -= 10
@@ -27,5 +45,4 @@ def arabicToRoman(arabicNumber):
         elif arabicNumber < 3000:
             romanNumeral = arabicToRomanNums[1000]
             arabicNumber -= 1000
-    print(romanNumeral)
     return romanNumeral
--- romanNumeralsTests.py
+++ romanNumeralsTests.py
@@ -1,21 +1,34 @@
 import unittest
-from romanNumerals import arabicToRoman
+from romanNumerals import arabicToRoman, romanToArabic
 
 
 class TestStringMethods(unittest.TestCase):
+    def test_romanToArabic(self):
+        self.assertEqual(1, romanToArabic("I"))
+        self.assertEqual(2, romanToArabic("II"))
+        self.assertEqual(3, romanToArabic("III"))
+        self.assertEqual(4, romanToArabic("IIII"))
+        self.assertEqual(5, romanToArabic("V"))
+        self.assertEqual(10, romanToArabic("X"))
+        self.assertEqual(20, romanToArabic("XX"))
 
     def test_arabicToRoman(self):
+        # 1   - 4   - 5   - 9   - 10
+        # 10  - 40  - 50  - 90  - 100
+        # 100 - 400 - 500 - 900 - 1000
         self.assertEqual("I", arabicToRoman(1))
         self.assertEqual("II", arabicToRoman(2))
         self.assertEqual("III", arabicToRoman(3))
-        self.assertEqual("IIII", arabicToRoman(4))
+        self.assertEqual("IV", arabicToRoman(4))
         self.assertEqual("V", arabicToRoman(5))
         self.assertEqual("VI", arabicToRoman(6))
         self.assertEqual("VII", arabicToRoman(7))
         self.assertEqual("VIII", arabicToRoman(8))
-        self.assertEqual("VIIII", arabicToRoman(9))
+        self.assertEqual("IX", arabicToRoman(9))
         self.assertEqual("X", arabicToRoman(10))
         self.assertEqual("XI", arabicToRoman(11))
+        self.assertEqual("XIV", arabicToRoman(14))
+        self.assertEqual("XIX", arabicToRoman(19))
         self.assertEqual("L", arabicToRoman(50))
         self.assertEqual("C", arabicToRoman(100))
         self.assertEqual("D", arabicToRoman(500))
