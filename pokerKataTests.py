740e46a7b0a289fc061ae1b32499716ca067954b
Lori Imbesi <lori.imbesi@gmail.com>
2021-09-30 21:31:33 -0400

    rewriting poker kata

--- pokerKataTests.py
+++ pokerKataTests.py
@@ -1,74 +1,30 @@
 import unittest
-from unittest import suite
-from pokerKata import Suit, parseCard, parseCards, Hand, parseHand, countFaces, Card, countSuits
+from pokerKata import Suit, parseCard
 
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_parseHand(self):
-        self.assertEqual(Hand.PAIR, parseHand(["3H", "5D", "4D", "3D", "8S"]))
-        self.assertEqual(Hand.THREE_OF_A_KIND, parseHand(
-            ["3H", "3S", "4D", "3D", "8S"]))
-        self.assertEqual(Hand.FOUR_OF_A_KIND, parseHand(
-            ["3H", "3S", "3C", "3D", "8S"]))
-        self.assertEqual(Hand.FLUSH, parseHand(
-            ["7S", "5S", "JS", "3S", "8S"]))
-        self.assertEqual(Hand.TWO_PAIR, parseHand(
-            ["7S", "3H", "7D", "3S", "8S"]))
-        self.assertEqual(Hand.FULL_HOUSE, parseHand(
-            ["7S", "7H", "7D", "8D", "8S"]))
-        self.assertEqual(Hand.STRAIGHT, parseHand(
-            ["3S", "2H", "5D", "4D", "6S"]))
-        self.assertEqual(Hand.STRAIGHT_FLUSH, parseHand(
-            ["9H", "6H", "7H", "8H", "5H"]))
-        self.assertEqual(Hand.ROYAL_FLUSH, parseHand(
-            ["AD", "KD", "TD", "JD", "QD"]))
+    # "2H" -> (2, Suit.HEARTS)
+    # def test_parseCard(self):
+    #     card = "2H"
+    #     (face, suit) = parseCard(card)
+    #     self.assertEqual(face, 2)
+    #     self.assertEqual(suit, Suit.HEARTS)
 
-    def test_countFaces(self):
-        cards = [Card(Suit.HEARTS, 3),
-                 Card(Suit.SPADES, 3),
-                 Card(Suit.DIAMONDS, 3),
-                 Card(Suit.HEARTS, 8),
-                 Card(Suit.HEARTS, 4)]
-        actual = countFaces(cards)
-        expected = {3: 3, 4: 1, 8: 1}
-        self.assertEqual(expected, actual)
-
-    def test_countSuits(self):
-        cards = [Card(Suit.HEARTS, 3),
-                 Card(Suit.SPADES, 3),
-                 Card(Suit.DIAMONDS, 3),
-                 Card(Suit.HEARTS, 8),
-                 Card(Suit.HEARTS, 4)]
-        actual = countSuits(cards)
-        expected = {Suit.HEARTS: 3, Suit.SPADES: 1,
-                    Suit.DIAMONDS: 1}
-        self.assertEqual(expected, actual)
-
-    def test_split(self):
-        input = "hi my name is"
-        output = input.split()
-        expected = ["hi", "my", "name", "is"]
-        self.assertEqual(expected, output)
-
-    def test_parseCards(self):
-        cards = parseCards(["3H", "4D", "5D"])
-        self.assertEqual(Suit.HEARTS, cards[0].suit)
-        self.assertEqual(3, cards[0].face)
-        self.assertEqual(Suit.DIAMONDS, cards[1].suit)
-        self.assertEqual(4, cards[1].face)
-        self.assertEqual(Suit.DIAMONDS, cards[2].suit)
-        self.assertEqual(5, cards[2].face)
-
-    def test_parseCard(self):
-        card = parseCard("3H")
-        self.assertEqual(Suit.HEARTS, card.suit)
-        self.assertEqual(3, card.face)
-
-        card = parseCard("5S")
-        self.assertEqual(Suit.SPADES, card.suit)
-        self.assertEqual(5, card.face)
+    def test_parseCard1(self):
+        card = "2H"
+        parsedCard = parseCard(card)
+        self.assertEqual(parsedCard, (2, Suit.HEARTS))
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+# Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH
+# Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S
+# Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH
+# Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH
+
+# Need to decide what goes in and what will come out of the function
+# String -> Tuple
