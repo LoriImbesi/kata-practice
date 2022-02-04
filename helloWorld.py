67ad3ed27dcc475233302a60a3b8fbe711592ea4
Lori Imbesi <lori.imbesi@gmail.com>
2022-02-03 21:37:32 -0500

    oddOrEven refresher

--- helloWorld.py
+++ helloWorld.py
@@ -3,3 +3,15 @@
 
 def sayHello(greeting):
     return greeting
+
+def multiplyTwoNumbers(int1, int2):
+    return int1 * int2
+
+def oddOrEven(int):
+    if int == 0:
+        return "0"
+    elif int % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+    
--- helloWorldTest.py
+++ helloWorldTest.py
@@ -1,12 +1,25 @@
 import unittest
 
-from helloWorld import sayHello
+from helloWorld import sayHello, multiplyTwoNumbers, oddOrEven
 
 
 class TestHelloWorld(unittest.TestCase):
 
     def test_sayHello(self):
         self.assertEqual("Hello World", sayHello("Hello World"))
+
+    def test_multiplyTwoNumbers(self):
+        self.assertEqual(12, multiplyTwoNumbers(3, 4))
+
+    def test_oddOrEven(self):
+        self.assertEqual("even", oddOrEven(10))
+        self.assertEqual("odd", oddOrEven(7))
+        self.assertEqual("0", oddOrEven(0))
+        self.assertEqual("odd", oddOrEven(-5))
+        self.assertEqual("even", oddOrEven(-4))
+        
+    
+    
 
 
 if __name__ == '__main__':
